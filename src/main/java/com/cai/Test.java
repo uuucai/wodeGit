f162d2d146546e082062bc2492b45a37b3060584
cai <cai@cai.com>
2022-04-12 14:14:34 +0800

    master2

--- src/main/java/com/cai/Test.java
+++ src/main/java/com/cai/Test.java
@@ -7,5 +7,6 @@ public class Test {
         System.out.println(888);
         System.out.println("hot-fix1");
         System.out.println("master1");
+        System.out.println("test commit");
     }
 }
